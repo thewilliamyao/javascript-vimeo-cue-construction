5180fe411cfd3607a67e8b4fcbf40ecc70240d12
wyao7 <wyao7@jhu.edu>
2017-01-03 03:18:12 -0800

    added comments

--- cue.js
+++ cue.js
@@ -8,8 +8,10 @@ var cueTime;
 var cueId;
 var videoDuration;
 var cueNumber = 0;
+var currentTime;
 
 function launchApp() {
+    //Create a new video player div
     var div = document.createElement("div");
     div.id = "main-video";
     document.getElementById("video-wrapper").appendChild(div);
@@ -21,9 +23,11 @@ function launchApp() {
 
     player = new Vimeo.Player('main-video', options);
 
+    //Switch scenes
     document.getElementById("menu").style.display = "none";
     document.getElementById("app").style.display = "block";
 
+    //Set duration for error checking later
     player.getDuration().then(function(duration) {
         videoDuration = duration;
     }).catch(function(error) {
@@ -31,9 +35,17 @@ function launchApp() {
     });
 
     player.on('play', function(data) {
+        player.getCurrentTime().then(function(seconds) {
+            currentTime = seconds;
+        });
+
         player.getCuePoints().then(function(cuePoints) {
-            if (cuePoints[0] && cuePoints[0].time == 0) {
-                document.getElementById("cue-display").innerHTML = cuePoints[0].data.customKey;
+            console.log("here");
+            //Player wipes cue display when played unless the video is set at time = 0 and there is a cue
+            if (cuePoints[0] && cuePoints[0].time == 0 && currentTime == 0) {
+                document.getElementById("cue-p").innerHTML = cuePoints[0].data.customKey;
+                document.getElementById("cue-p").style.display = "inline";
+                document.getElementById("cue-link").style.display = "none";
             } else {
                 document.getElementById("cue-display").style.display = "none";
             }
@@ -41,6 +53,7 @@ function launchApp() {
     });
 
     player.on('cuepoint', function(data) {
+        //cue is displayed differently depending on if it is a link
         if (isURL(data.data.customKey)) {
             document.getElementById("cue-p").style.display = "none";
 
@@ -58,6 +71,7 @@ function launchApp() {
 }
 
 function restartApp() {
+    //Switches Scene
     document.getElementById("menu").style.display = "block";
     document.getElementById("app").style.display = "none";
 
@@ -76,6 +90,7 @@ function addCue() {
     cueText = document.getElementById("input-text").value;
     cueTime = document.getElementById("input-time").value;
 
+    //Splits time by ':' to get mm and ss
     var cueTimeSplit = cueTime.split(':');
     var cueTimeInt = (+cueTimeSplit[0]) * 60 + (+cueTimeSplit[1]);
 
@@ -96,6 +111,7 @@ function addCue() {
     }
 }
 
+//Check if Cue is valid
 function validCue(cueTimeInt) {
     if (cueText == "") {
         document.getElementById("error-message").innerHTML = "Please enter a message for the cue!";
@@ -117,6 +133,7 @@ function createCue() {
     div.className = "cue-wrapper";
     div.id = cueNumber.toString();
 
+    //Creates a 'a' tag for link and a 'p' else wise
     if (isURL(cueText)) {
         var cueLink = document.createElement("a");
         cueLink.className = "textBlock";
@@ -141,6 +158,7 @@ function createCue() {
 
 }
 
+//Attaches the cueId to the onclick function to help with player.removeCuePoint
 function addRemoveButton() {
     var removeId = cueId;
     var divId = cueNumber.toString();
@@ -165,6 +183,7 @@ function removeCue(removedCue) {
     });
 }
 
+//Will return true if string starts with www or http
 function isURL(str) {
     if (/^www./.test(str)) {
         str = "http://" + str;
